6b1947d6a22216557cdd8cca7d7f8753788c5d2d
KatepFigueiredo <catarinafigueiredo126@gmail.com>
2026-01-21 23:29:29 +0000

    Simplify Vercel config to point directly to app.py

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Adiciona o diretório pai (onde está o app.py) ao path do Python
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/..')
-
-from app import app # O Vercel procura por uma variável chamada 'app' ou 'handler'
